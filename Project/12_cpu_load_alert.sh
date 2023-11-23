081250cccce549907819584e7af49f173f8177b8
vivaan19 <shiromanivivan@gmail.com>
2023-11-23 12:17:50 +0530

    [ADD] CPU Load Alert file

---
+++ Project/12_cpu_load_alert.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+#############################################################################
+# This script is used to alert the user if the load average is greater than 1
+#############################################################################
+
+cat << EOF
+
+    ####################################
+    Welcome $(whoami) to cpu load alert
+    ####################################
+
+EOF
+
+LOAD_AVG_VAL_1=$(top -bn1 | grep load | awk -F"  " '{print $3}' | awk -F": " '{print $2}' | awk -F', ' '{print $1}')
+
+if [[ $(echo "$LOAD_AVG_VAL_1 > 1" | bc) -eq 1 ]]; then
+
+
+    echo "WARNING: LOAD AVERAGE IS GREATER THAN 1"
+    exit 1
+
+else
+    echo "SYSTEM LOAD AVERAGE FINE"
+    exit 0
+fi
