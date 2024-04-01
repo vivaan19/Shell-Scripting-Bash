6cb5fd361689cfc17a5b3b581af8275dcf19b57d
vivaan19 <shiromanivivan@gmail.com>
2024-04-01 18:56:31 +0530

    [IMP] added important function comments - in dbConfig added db exist validation

--- Project/EcomDeploy-sh/ecom_deploy.sh
+++ Project/EcomDeploy-sh/ecom_deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
-#####################################################################################
+###############################################################################################
 # Author ::: Vivaan Shiromani 
 # Date   ::: 27/03/2024
 # About  ::: Multi-os (apt and yum) localhost LAMP stack E-commerce application deployment script 
-######################################################################################
+#################################################################################################
 
 DEBIAN_PKG_LST=(git firewalld apache2 mysql-server php php-mysql)
 CENTOS_PKG_LST=(git firewalld httpd mariadb-server php php-mysqlnd) 
@@ -16,6 +16,57 @@ PORT_ARR=(80 3306)
 
 DB_HOST="localhost"
 
+#########################################################################################
+# This function is used to update and upgrade the system using yum or apt package manager 
+# Also to install various packages which will be used to for further processing 
+# Arguments :: 
+# $1 -- This is the package string -- either yum or apt 
+# $2 -- Package array which will install package one-by-one here also array value is passed through value 
+##########################################################################################
+
+checkPackage() { 
+
+    pkg_name=$1
+
+    echo "INFO ####################### UPDATING SYSTEM"
+    if ! sudo "$pkg_name" update -y &> /dev/null && sudo "$pkg_name" upgrade -y &> /dev/null; then 
+
+        echo "ERROR: UPDATING SYSTEM"
+        exit 1
+    fi
+
+    # Installing required packages 
+    # local pkg_arr=("$@")
+    
+    echo "INFO ####################### INSTALLING PACKAGES"
+    # take reference of array 
+
+    shift
+
+    # local -n pkg_arr=$2 local -n is not used in bash version upto 4         
+    
+    pkg_arr=("$@")        
+
+    for pkg in "${pkg_arr[@]}"; do
+
+        if ! sudo "$pkg_name" install "$pkg" -y; then 
+
+            echo "ERROR: INSTALLING PACKAGE - $pkg"
+            exit 1 
+        fi
+    done
+
+}
+
+#########################################################################################################
+# This function is used to start the service one-by-one as per list provided for either OS 
+# And adding port of apache web server which is 80 and mysql which is 3306 to firewalld using firewall-cmd 
+# Arguments : 
+# $1 -- "s" or "p" to differentiate the function is working for service or port 
+# arr -- this the array which is passed by value as ${array_name[@]} to the function and to access using this ("$@")
+#
+# This function also uses shift to change to second argument which is array after first argument 
+##########################################################################################################
 checkServicePort() {
 
     func_type=$1
@@ -65,42 +116,17 @@ checkServicePort() {
 
 }
 
-checkPackage() { 
-
-    pkg_name=$1
-
-    echo "INFO ####################### UPDATING SYSTEM"
-    if ! sudo "$pkg_name" update -y &> /dev/null && sudo "$pkg_name" upgrade -y &> /dev/null; then 
-
-        echo "ERROR: UPDATING SYSTEM"
-        exit 1
-    fi
-
-    # Installing required packages 
-    # local pkg_arr=("$@")
-    
-    echo "INFO ####################### INSTALLING PACKAGES"
-    # take reference of array 
-
-    # Todo --- local -n error in centos 
-
-    shift
-
-    # local -n pkg_arr=$2 local -n is not used in bash version upto 4         
-    
-    pkg_arr=("$@")        
-
-    for pkg in "${pkg_arr[@]}"; do
-
-        if ! sudo "$pkg_name" install "$pkg" -y; then 
-
-            echo "ERROR: INSTALLING PACKAGE - $pkg"
-            exit 1 
-        fi
-    done
-
-}
-
+#######################################################################
+# This function is used to make load .sql files to mysql to 
+# create database, user on localhost and give various
+# privileges. Also this function creates product table in the database  
+# where all details including images of products is loaded 
+# Arguments:: 
+# $1 -- sql file name to have different .sql file to run 
+# $2 -- Database name 
+# $3 -- User name 
+# $4 -- Database password 
+#########################################################################
 dbCommand() {
 
     if [ "$1" == "db_sql_commands" ]; then 
@@ -135,20 +161,28 @@ fi
     fi 
 }
 
+####################
+# This function configures the database by creating user, database and password 
+# This function calls dbCommand function 
+# It has a while loop which gives users chance to re-enter new user name and database name 
+
+# Args :: None 
+####################
 databaseConfig() {
 
     while true; do
         
-        read -rp "Enter Datbase Username: " DB_USER
+        read -rp "Enter Database Username: " DB_USER
+        read -rp "Enter Database Name: " DB_NAME
         
-        if_exists=$(sudo mysql -sN -e "SELECT COUNT(*) FROM mysql.user WHERE User = '${DB_USER}'")
-        
+        if_user_exists=$(sudo mysql -sN -e "SELECT COUNT(*) FROM mysql.user WHERE User = '${DB_USER}'")
+        if_db_exists=$(sudo mysql -sN -e "SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='${DB_NAME}'")
+
         # Check if database user already exists - if count of the user is 1 then the user exists 
-        if [[ $if_exists -lt 1 ]]; then
+        if [[ $if_user_exists -eq 0 && $if_db_exists -eq 0 ]]; then
 
-            echo "INFO ############## DATBASE USER DOES NOT EXIST" 
+            echo "INFO ############## DATABASE AND USER DOES NOT EXIST" 
 
-            read -rp "Enter Database Name: " DB_NAME 
             read -rsp "Enter Database Password: " DB_PASSWD
         
             dbCommand "db_sql_commands" "$DB_NAME" "$DB_USER" "$DB_PASSWD" 
@@ -157,80 +191,59 @@ databaseConfig() {
             break
 
         else 
-            echo "ERROR DATBASE USER EXISTS : CREATE A NEW USER FIRST" 
+            echo "ERROR ############## DATABASE AND USER EXISTS" 
+
+            # if_product_t_exists = $(sudo mysql -sN -e "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$DB_NAME' AND table_name LIKE 'products';")
+            
+            # if [ $if_product_t_exists -eq 0 ]; then
+                
+            #     echo "CREATING PRODUCT TABLE ---- " 
+            #     dbCommand "db_load_script"
+            
+            # else
+
+            #     # Here I want to get search for the password for the user and place into DB_PASSWD Variable 
+        
         fi
 
     done
-     
-    # here pipe statements cannot have a single exit code so pipestatus array variable is used to 
-    # seperate command exit codes one-by-one 
-
-    # Load Product Inventory Information to database
-
-        # PIPESTATUS array has all exit codes for each command in the pipe 
-    #     if ! sudo cat db-load-script.sql | mysql; then
-        
-    #         if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-    #             echo "ERROR: Failed to read product db script"
-        
-    #         elif [[ ${PIPESTATUS[1]} -ne 0 ]]; then
-
-    #             echo "ERROR: Failed to execute product MySQL db command"
-    #         fi
-    #         exit 1
-        
-    #     fi
-
-}
-
-serviceRestart() {
-
-    if [ $1 == "apt" ]; then
-
-        if ! sudo systemctl restart apache2; then
-
-            echo "ERROR: ALL PROCESS DONE BUT NOT ABLE TO RESTART APACHE2 SERVICE"
-            exit1 
-        fi
-
-    elif [ $1 == "yum" ]; then
-        if ! sudo systemctl restart httpd; then
-
-            echo "ERROR: ALL PROCESS DONE BUT NOT ABLE TO RESTART HTTPD SERVICE"
-            exit1 
-        fi
-    fi
-
-}
-
-webSetup() {
-
-    # Pkg manager specific functions 
-
-    if [ "$1" == "apt" ]; then
-
-        echo "INFO: APT BASED LAMP STACK E-COMMERCE APP INSTALLATION"
-
-        # Update and Upgrade system 
-        checkPackage "apt" ${DEBIAN_PKG_LST[@]}
-        
-        # Check Start and Enable Services  
-        checkServicePort "s" ${DEBIAN_SERVICE_LST[@]}
     
-    elif [ "$1" == "yum" ]; then
-        
-        echo "INFO: YUM BASED LAMP STACK E-COMMERCE APP INSTALLATION"
 
-        # Update and Upgrade system 
-        checkPackage "yum" ${CENTOS_PKG_LST[@]}
-        
-        # Check Start and Enable Services  
-        checkServicePort "s" ${CENTOS_SERVICE_LST[@]}
 
-        # Change DirectoryIndex index.html to DirectoryIndex index.php to make the php page the default page
-        sudo sed -i 's/index.html/index.php/g' /etc/httpd/conf/httpd.conf
+    # Usage of Pipe-status command  
+    #############
+        # here pipe statements cannot have a single exit code so pipestatus array variable is used to 
+        # seperate command exit codes one-by-one 
 
-    fi 
+        # Load Product Inventory Information to database
+
+            # PIPESTATUS array has all exit codes for each command in the pipe 
+        #     if ! sudo cat db-load-script.sql | mysql; then
+            
+        #         if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+        #             echo "ERROR: Failed to read product db script"
+            
+        #         elif [[ ${PIPESTATUS[1]} -ne 0 ]]; then
+
+        #             echo "ERROR: Failed to execute product MySQL db command"
+        #         fi
+        #         exit 1
+            
+        #     fi
+    ##############
+
+
+}
+
+###############################################################################
+# This function do's the main web configuration 
+# It has all the common functions of yum and apt based os 
+# like this function adds the relevant port to firewalld, clone code, 
+# copy the code and replaces string in index.php file inside /var/www/html 
+# Args ::: 
+# NONE 
+##################################################################################
+webConfig() {
 
     # COMMON FUNCTIONS .... 
     
@@ -263,22 +276,81 @@ webSetup() {
     if [ -f "$PHP_FNAME" ]; then
 
         # first comment the strings 
-                
         sudo sed -i "s#\$link = mysqli_connect(\$dbHost, \$dbUser, \$dbPassword, \$dbName);#//#g" $PHP_FNAME 
         sudo sed -i "s#\$dbHost#//\$dbHost#g"  $PHP_FNAME
         sudo sed -i "s#\$dbUser#//\$dbUser#g"  $PHP_FNAME
         sudo sed -i "s#\$dbPassword#//\$dbPassword#g"  $PHP_FNAME
         sudo sed -i "s#\$dbName#//\$dbName#g"  $PHP_FNAME
         sudo sed -i "s#// \$link = mysqli_connect('172.20.1.101', 'ecomuser', 'ecompassword', 'ecomdb')#\$link('$DB_HOST', '$DB_USER', '$DB_PASSWD', '$DB_NAME')#g" $PHP_FNAME
-        
-
-        
+         
     fi
+
+}
+
+######################################################################################
+# This function restarts the apache2 or httpd service after all operations performed
+# Args :::: 
+# $1 -- Package name -- yum or apt  
+######################################################################################
+serviceRestart() {
+
+    if [ $1 == "apt" ]; then
+
+        if ! sudo systemctl restart apache2; then
+
+            echo "ERROR: ALL PROCESS DONE BUT NOT ABLE TO RESTART APACHE2 SERVICE"
+            exit1 
+        fi
+
+    elif [ $1 == "yum" ]; then
+        if ! sudo systemctl restart httpd; then
+
+            echo "ERROR: ALL PROCESS DONE BUT NOT ABLE TO RESTART HTTPD SERVICE"
+            exit1 
+        fi
+    fi
+
+}
+
+######################################################################
+# Main web setup function which calls all other function in the script 
+######################################################################
+webSetup() {
+
+    # Pkg manager specific functions 
+
+    if [ "$1" == "apt" ]; then
+
+        echo "INFO: APT BASED LAMP STACK E-COMMERCE APP INSTALLATION"
+
+        # Update and Upgrade system 
+        checkPackage "apt" ${DEBIAN_PKG_LST[@]}
+        
+        # Check Start and Enable Services  
+        checkServicePort "s" ${DEBIAN_SERVICE_LST[@]}
+    
+    elif [ "$1" == "yum" ]; then
+        
+        echo "INFO: YUM BASED LAMP STACK E-COMMERCE APP INSTALLATION"
+
+        # Update and Upgrade system 
+        checkPackage "yum" ${CENTOS_PKG_LST[@]}
+        
+        # Check Start and Enable Services  
+        checkServicePort "s" ${CENTOS_SERVICE_LST[@]}
+
+        # Change DirectoryIndex index.html to DirectoryIndex index.php to make the php page the default page
+        sudo sed -i 's/index.html/index.php/g' /etc/httpd/conf/httpd.conf
+
+    fi 
+
+    webConfig 
+
 }
 
 
+# Multi-os setup for apt based and yum based os 
 if apt --help &> /dev/null; then 
-    
     webSetup "apt"
     serviceRestart "apt" 
 
